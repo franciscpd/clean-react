a75c2fbb8a123406ef3cedad7621ceae5739de4d
Francisross Soares de Oliveira <franciscpd@gmail.com>
2021-03-22 01:34:45 -0300

    feat: ensure EmailValidation returns error if email is invalid

--- src/validation/validators/required-field/index.ts
+++ src/validation/validators/required-field/index.ts
@@ -2,7 +2,7 @@ import { FieldValidation } from '@/validation/protocols'
 import { RequiredFieldError } from '@/validation/errors'
 
 export class RequiredFieldValidation implements FieldValidation {
-  constructor (readonly fieldName) {}
+  constructor (readonly field) {}
 
   validate (value: string): Error {
     return value ? null : new RequiredFieldError()
